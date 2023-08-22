fb0896a8e056983345e9136dc77028cb10972e1a
Douglas Rodrigues Pedro <douglasrpdc1@gmail.com>
2023-08-22 10:28:49 -0300

    Desenvolvimento da classe qualquer q eu esqueci de salvar antes ;)

---
+++ src/javaprojetovmcteste/model/Qualquer.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaprojetovmcteste.model;
+
+/**
+ *
+ * @author 182210135
+ */
+public class Qualquer {
+    
+    private int id;
+    private String nome;
+    private String cpf;
+
+    public Qualquer(int id, String nome, String cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
